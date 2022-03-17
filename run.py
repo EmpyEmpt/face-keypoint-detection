239fc582bac278ecc560ff6ca8c43000c5997971
EmpyEmpt <3mptinessy@gmail.com>
2022-03-17 14:58:18 +0400

    working model (85 acc)

--- run.py
+++ run.py
@@ -1,11 +1,21 @@
-from face_finder import crop_face
 import tensorflow as tf
 import numpy as np
 import config as cfg
 import csv
 import pandas as pd
 from PIL import Image
+import os
 import cv2
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
 
 
 class image:
@@ -35,8 +45,14 @@ class image:
             if i == 0:
                 i += 1
                 continue
-            all.append(image(row[0], int(row[1]), int(
-                row[2]), int(row[3]), int(row[5]), row[5:]).to_list())
+            ye = True
+            for value in row:
+                if value.startswith('-'):
+                    ye = False
+                    break
+            if ye:
+                all.append(image(row[0], int(row[1]), int(
+                    row[2]), int(row[3]), int(row[5]), row[5:]).to_list())
         file.close()
         return all
 
@@ -60,20 +76,28 @@ def split_dataset(dataset, test_ratio=0.20):
     return dataset[~test_indices], dataset[test_indices]
 
 
+def crop_face(image, image_path, x, y, w, h):
+    roi_color = image[y:y + h, x:x + w]
+    return roi_color
+
+
 def prep_image(dp):
-    image = dp[0]
+    image_path = dp[0]
     bb = dp[1]
     wh = dp[2]
     points = dp[3]
-
     # crop out face from image
-    imagepath = cv2.imread(cfg.IMAGES_PATH + image)
-    _, path = crop_face(imagepath,
-                        int(bb[0]), int(bb[1]), int(wh), int(wh))
+    image = cv2.imread(cfg.IMAGES_PATH + image_path)
+    image = crop_face(image, image_path, int(
+        bb[0]), int(bb[1]), int(wh), int(wh))
+    path = 'data\data\\tmp\\' + image_path
+    cv2.imwrite(path, image)
 
     # resize image to xxx * xxx
     image = Image.open(path)
     image = image.resize((194, 194))
+
+    path = 'data\data\\croped\\' + image_path
     image.save(path)
 
     # save resize ratio
@@ -86,10 +110,6 @@ def prep_image(dp):
 
     # convert points to relative [0.0 -> 1]
     npo = []
-    # for i in range(0, 135, 2):
-    #     point1 = float(points[i]) / wh
-    #     point2 = float(points[i+1]) / wh
-    #     npo.append([point1, point2])
     for point in points:
         point_ = float(point) / wh
         npo.append(point_)
@@ -99,6 +119,47 @@ def prep_image(dp):
 
     # PROFIT?
     return path, npo
+
+
+def build():
+    # initialize the model along with the input shape to be
+    # "channels last" and the channels dimension itself
+    model = Sequential()
+    inputShape = (194, 194, 3)
+    # chanDim = -1
+    # if we are using "channels first", update the input shape
+    # and channels dimension
+    # if K.image_data_format() == "channels_first":
+    #     inputShape = (3, 194, 194)
+    #     chanDim = 1
+    # CONV => RELU => POOL
+    model.add(Conv2D(32, (3, 3), padding="same",
+                     input_shape=inputShape))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+    model.add(Dropout(0.25))
+    # (CONV => RELU) * 2 => POOL
+    model.add(Conv2D(64, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(Conv2D(64, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    # (CONV => RELU) * 2 => POOL
+    model.add(Conv2D(128, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(Conv2D(128, (3, 3), padding="same"))
+    model.add(Activation("relu"))
+    model.add(Dropout(0.25))
+    # first (and only) set of FC => RELU layers
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation("relu"))
+    model.add(Dropout(0.5))
+    # softmax classifier
+    model.add(Dense(136))
+    # return the constructed network architecture
+    return model
 
 
 def main():
@@ -116,28 +177,56 @@ def main():
     testY = []
 
     # for i in range(train_ds.shape[0]):
-    for i in range(2):
+    for i in range(train_ds.shape[0]):
         path, points = prep_image(train_ds.iloc[i])
         input_image = tf.io.read_file(path)
-        input_image = tf.image.decode_jpeg(input_image)
+        input_image = tf.image.decode_image(input_image)
         input_image = tf.cast(input_image, dtype=tf.int32)
         trainX.append(input_image)
         trainY.append(points)
 
         # VERY TEMPOPARY BREAK
-        break
+        # break
 
-    # for i in range(test_ds.shape[0]):
-    for i in range(2):
+    for i in range(test_ds.shape[0]):
         path, points = prep_image(train_ds.iloc[i])
         input_image = tf.io.read_file(path)
-        input_image = tf.image.decode_jpeg(input_image)
+        input_image = tf.image.decode_image(input_image)
         input_image = tf.cast(input_image, dtype=tf.int32)
         testX.append(input_image)
         testY.append(points)
 
         # VERY TEMPOPARY BREAK
-        break
+        # break
+    trainX_ = tf.convert_to_tensor(trainX, dtype=tf.int32)
+    trainY_ = tf.convert_to_tensor(trainY, dtype=tf.float32)
+
+    testX_ = tf.convert_to_tensor(testX, dtype=tf.int32)
+    testY_ = tf.convert_to_tensor(testY, dtype=tf.float32)
+    checkpoint_path = "training_1/cp-{epoch:04d}.ckpt"
+
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    batch_size = 32
+
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path,
+        verbose=1,
+        save_weights_only=True,
+        save_freq=5*batch_size)
+
+    # model = SmallerVGGNet.build()
+    model = build()
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.MeanAbsoluteError(),
+                  metrics=['accuracy'])
+    model.fit(trainX_,
+              trainY_,
+              epochs=50,
+              batch_size=batch_size,
+              validation_data=(testX_, testY_),
+              callbacks=[cp_callback])
+    model.save('saved_model/model_1')
 
 
 if __name__ == '__main__':
