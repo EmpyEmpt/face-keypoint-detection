6d6340476c79207a400f3e501758369cbcbb65b4
EmpyEmpt <3mptinessy@gmail.com>
2022-04-07 01:43:09 +0400

    Updated model architecture

--- model.py
+++ model.py
@@ -1,71 +1,76 @@
 import tensorflow as tf
-import os
 import config as cfg
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Activation
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import GlobalAveragePooling2D
 
 
-def build(input_dim_size, output=cfg.OUTPUTS):
-    model = Sequential()
+def build(image_size=cfg.IMAGE_SIZE, outputs=cfg.OUTPUTS):
+    input_shape = (image_size, image_size, 3)
 
-    # model.add(BatchNormalization(input_shape=(input_dim_size, input_dim_size, 3)))
-    model.add(Conv2D(24, (5, 5), padding='same',
-              input_shape=(input_dim_size, input_dim_size, 3)))
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), padding='same',
+              input_shape=input_shape))
+    model.add(BatchNormalization(input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
 
-    model.add(Conv2D(36, (5, 5)))
+    model.add(Conv2D(64, (5, 5)))
+    model.add(BatchNormalization(input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2),
               strides=(2, 2), padding='valid'))
 
     model.add(Conv2D(48, (5, 5)))
+    model.add(BatchNormalization(input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2),
               strides=(2, 2), padding='valid'))
 
     model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization(input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2),
               strides=(2, 2), padding='valid'))
 
-    model.add(Conv2D(64, (3, 3)))
+    model.add(Conv2D(128, (3, 3)))
+    model.add(BatchNormalization(input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(GlobalAveragePooling2D())
 
-    model.add(Dense(1000, activation='relu'))
     model.add(Dense(500, activation='relu'))
-    model.add(Dense(output))
+    model.add(Dense(outputs))
     return model
 
 
-def compile_model(size, load_latest=False,  checkpoint_path=cfg.CHECKPOINT_PATH):
-    model = build(size)
-    model.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
+def compile_model(image_size=cfg.IMAGE_SIZE):
+    model = build(image_size)
 
-    if load_latest:
-        checkpoint_dir = os.path.dirname(checkpoint_path)
-        latest = tf.train.latest_checkpoint(checkpoint_dir)
-        model.load_weights(latest)
+    model.compile(
+        loss=tf.keras.losses.mean_squared_error,
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+        metrics=['mse'])
 
     return model
 
 
-def load_model(path):
+def load_model(path=cfg.MODEL_PATH):
     model = tf.keras.models.load_model(path)
+    return model
+
+
+def load_weights(model, path):
+    model.load_weights(path)
     return model
 
 
 def evaluate_model(model, x, y):
     loss, acc = model.evaluate(x, y, verbose=2)
-    print(f'Model, accuracy: {acc*100:5.2f}%')
+    print(f'Model accuracy: {acc*100:5.2f}%')
 
 
 def save_model(model, path):
