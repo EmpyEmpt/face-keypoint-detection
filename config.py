b0520712c7379b0cd2e624303cdc6cdb15523169
EmpyEmpt <3mptinessy@gmail.com>
2022-04-07 01:44:21 +0400

    Renamed 'model' to 'model_path'

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ DATASET_PATH = 'data/data/images/'
 IMAGE_SIZE = 128
 OUTPUTS = 136
 CHECKPOINT_PATH = 'training/cp-{epoch:04d}.ckpt'
-MODEL_NAME = 'model.h5'
+MODEL_PATH = 'model.h5'
 COMPRESSED_PATH = 'data/data/compressed/'
